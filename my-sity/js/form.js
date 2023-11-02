47b03f3414c5cd916b58285e2b4d49a36f16bbc3
irenka123 <69099627+irenka123@users.noreply.github.com>
2023-11-02 03:49:39 +0100

    Add files via upload

---
+++ my-sity/js/form.js
@@ -0,0 +1,28 @@
+const userNameField=document.querySelector('#user-name');
+const userEmailField=document.querySelector('#user-email');
+const formSubmitBtn=document.querySelector('#form-submit-btn');
+const formContainer=document.querySelector('#form');
+
+formContainer.addEventListener('submit',function(event){
+event.preventDefault();
+});
+formSubmitBtn.addEventListener('click', function() {
+    const formData= new FormData(formContainer);
+
+    console.log('Дані форми готуються до відправки!');
+
+    fetch('/', {
+        method:"POST",
+        headers:{"Content-Type":"application/x-www-form-urlencoded"},
+        body:new URLSearchParams(formData).toString()
+
+    }).then(function(){
+        alert('Дякуємо, що заповнили форму!');
+    }).catch(function(){
+        alert('Помилка при відправці форми!');
+    })
+
+})
+
+
+
